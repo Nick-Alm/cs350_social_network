b2be56c9b2fe5b0c30778fcfa2f30b9a98522932
Nick-Alm <alm.nicholas.2013@gmail.com>
2017-11-20 23:35:52 +0000

    minor change to mongodb. This will not work without the connection password

--- mongo/mongodb.js
+++ mongo/mongodb.js
@@ -4,7 +4,7 @@ This file contains the information necessary to create the connection to the mon
  **/
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://nick-alm:Eye45678@ds137090.mlab.com:37090/mytest', { useMongoClient: true });
+mongoose.connect('mongodb://nick-alm:password@ds137090.mlab.com:37090/mytest', { useMongoClient: true });
 mongoose.Promise = global.Promise;
 
 var user = require('./user_model.js');
