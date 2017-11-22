632611c58a1f2c296b9e60763b742a8360ef3f26
Nick-Alm <alm.nicholas.2013@gmail.com>
2017-11-22 15:11:05 +0000

    created post model

--- application_test.js
+++ application_test.js
@@ -1,29 +1,21 @@
 var mongoDB = require('./mongo/mongodb.js');
 var user = require('./mongo/user_model.js');
-
+var post = require('./mongo/post_model.js');
 
 
 
 //test to create a new user:
 
 function test_create_user(first, last, password, email){
-    var new_user = new user({ first_name: first, last_name: last, password: password, email: email });
-    
-    new_user.save(function (err) {
-      if (err) {
-        console.log(err);
-      } else {
-          
-        console.log('successfully created user with id: ' );
-        
-        var query = user.findOne({ last_name: last }); //define the mongoose query to search the collection
-        
-        query.exec(function (err, person) { // actually execute the query
-            if (err) return handleError(err);
-                console.log(person._id) // log the _id of the found object in the console in order to verify that something was actually found
-            })
-      }
-    });
+    console.log("Testing user creation");
+    user.create_user(first, last, password, email);
+
+}
+
+function test_create_post(creator_id, title, body){ //TODO <--
+    console.log("Testing post creation");
+    post.create_post(creator_id, title, body);
+
 }
 
 
@@ -34,5 +26,5 @@ function test_create_friendship(user_id_one, user_id_two){
 
 
 // RUN ALL TESTS:
-//test_create_user("John","Doe","testpassword123","test@test.com");
+test_create_post("5a13493450c4400509b8c638","first Post","Here is the body - Shannon Demo");
 //test_create_friendship("5a13493450c4400509b8c638", "5a1349cf40dad8051baae596");
--- mongo/user_model.js
+++ mongo/user_model.js
@@ -30,4 +30,25 @@ user.create_friendship = function(this_user_id, new_friend_id){
         }
     );
 }
+
+user.create_user = function(first, last, password, email){
+    var new_user = new user({ first_name: first, last_name: last, password: password, email: email });
+    
+    new_user.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+          
+        console.log('successfully created user with id: ' );
+        
+        var query = user.findOne({ last_name: last }); //define the mongoose query to search the collection
+        
+        query.exec(function (err, person) { // actually execute the query
+            if (err) return handleError(err);
+                console.log(person._id) // log the _id of the found object in the console in order to verify that something was actually found
+            })
+      }
+    });
+}
+
 module.exports = user;
